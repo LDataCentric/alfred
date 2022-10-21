45ddc048181fd4dfa31b8c3a70df0d7c228d08b9
Ibis Prevedello <ibiscp@gmail.com>
2022-10-20 22:34:49 -0300

    refac: use alfred to run the project

--- util/constants.py
+++ util/constants.py
@@ -1,16 +1,16 @@
 # paths are inside the container so the volume needs to be connected
-BACKUP_DIR = "/refinery/backup"
+BACKUP_DIR = "refinery/backup"
 CONNECTION_STRING = "postgresql://postgres:onetask@graphql-postgres:5432"
-DOCKER_COMPOSE = "/refinery/docker-compose.yml"
+DOCKER_COMPOSE = "refinery/docker-compose.yml"
 DOCKER_COMPOSE_TEMPLATE = "templates/docker-compose.yml"
 EXEC_ENVS = {
-    "AC_EXEC_ENV": "kernai/refinery-ac-exec-env",
-    "ML_EXEC_ENV": "kernai/refinery-ml-exec-env",
-    "LF_EXEC_ENV": "kernai/refinery-lf-exec-env",
-    "RECORD_IDE_ENV": "kernai/refinery-record-ide-env",
+    "AC_EXEC_ENV": "ibiscp/ac-exec-env",
+    "ML_EXEC_ENV": "ibiscp/ml-exec-env",
+    "LF_EXEC_ENV": "ibiscp/lf-exec-env",
+    "RECORD_IDE_ENV": "ibiscp/record-ide-env",
 }
-JWKS_PATH = "/refinery/oathkeeper/jwks.json"
+JWKS_PATH = "refinery/oathkeeper/jwks.json"
 PG_CONTAINER = "graphql-postgres"
 POSTGRES_MIGRATE_CONTAINER = "postgres-migrate"
-SERVICE_VERSIONS = "/refinery/versions.json"
-SETTINGS = "/refinery/settings.json"
+SERVICE_VERSIONS = "refinery/versions.json"
+SETTINGS = "refinery/settings.json"
